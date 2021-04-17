d870897a2fa7db4e554e25d1509a0dc05ec9ee3d
Choi-Jungwoo <jungwoo@cineuse.com>
2021-04-18 01:34:09 +0800

    completed

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,28 @@
 
 use panic_halt as _;
 use esp8266_hal::prelude::*;
+use esp8266_hal::target::Peripherals;
+use esp8266_hal::ehal::digital::v2::{InputPin, OutputPin};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let dp = Peripherals::take().unwrap();
+
+    let pins = dp.GPIO.split();
+    let mut led = pins.gpio5.into_push_pull_output();
+    let fire = pins.gpio4.into_floating_input();
+
+    let (mut timer, _) = dp.TIMER.timers();
+
+    led.set_low().unwrap();
+
+    loop {
+        if fire.is_high().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+
+        timer.delay_ms(50);
+    }
 }
